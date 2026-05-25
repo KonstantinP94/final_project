d333d2b7ff3b4b1f2c4ac10eb9050f4035654659
Поздняков <konstantin.p.94@mail.ru>
2026-05-25 09:28:02 +0300

    доделать

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
     first_name = forms.CharField(max_lenght=30, required=True, label='Имя')
     last_name= forms.CharField(max_lenght=30, required=True, label='Фамилия')
@@ -17,3 +17,4 @@ class CustomUserCreationForm(UserCreationForm):
             'password1',
             'password2'
         ]
+        
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import forms
 
-# Create your views here.
+
+def sign_up(request):
+   form = SignUpForm(request.POST or None)
+   if request.method == "POST" and form.is_valid():
+       user = form.save()
+		login(request, user)
+       return redirect('shop:products')
+   return render(request, 'sign_up.html', {'form': form})
+
+
+
