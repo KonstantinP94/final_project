eaf2d3a4367c50c35c0c7c982238664167f1ae43
Константин Поздняков <konstantin9213@yandex.ru>
2026-05-26 19:08:29 +0300

    Добавил функцию register для формы регистрации в - users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('sport:home')
+        # если форма невалидна, то выходим из if и в конце render покажет форму с ошибками
+    else:
+        form = CustomUserCreationForm()   # пустая форма для GET
+
+    return render(request, 'register.html', {'form': form})
