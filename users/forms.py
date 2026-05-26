4f6c6f011b55b8fbb04f0c96b0ccd5d245c4868b
Поздняков Константин Александрович <pozdniakov_k@rusklimat.ru>
2026-05-26 18:27:50 +0300

    users/forms.py Исправил CustomUserCreationForm и Meta в нём

--- users/forms.py
+++ users/forms.py
@@ -4,15 +4,10 @@ from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
-    first_name = forms.CharField(max_lenght=30, required=True, label='Имя')
-    last_name= forms.CharField(max_lenght=30, required=True, label='Фамилия')
     
     class Meta:
         model = User
         fields = [
-            'username',
-            'first_name',
-            'last_name',
             'email',
             'password1',
             'password2'
