8a09935f12fd156c4f8fdb001f5f4bb39917a60e
Поздняков <konstantin.p.94@mail.ru>
2026-05-31 00:59:35 +0300

    добавлен RedirectView в корневой urls.py

--- final_project/urls.py
+++ final_project/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sport/', include(('sport.urls', 'sport'), namespace='sport')),
     path('users/', include(('users.urls', 'users'), namespace='users')),
+    path('', RedirectView.as_view(url='/sport/home/', permanent=False)),
     
 ]
 
