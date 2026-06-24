70c239304d401c7fa95798c6a35b7efcb6946ec7
Поздняков <konstantin.p.94@mail.ru>
2026-06-25 00:20:59 +0300

    Добавление отзыва

--- sport/urls.py
+++ sport/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('trainer/<int:trainer_id>/', views.trainer_detail, name='trainer_detail'),
     path('book_slot/<int:slot_id>/', views.book_slot, name='book_slot'),
-    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'), 
+    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('add_review/<int:trainer_id>/', views.add_review, name='add_review'),
     
 ]
